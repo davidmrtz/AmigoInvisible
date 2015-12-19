17267928ca24ac1c9ef000cfe73bead4c8047f7c
David Martínez <d.19mar@gmail.com>
2015-12-19 14:33:39 +0100

    Pantalla inicio, menú principal y control para volver HECHOS

---
+++ TuAmi/TuAmi/CrearGrupoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CrearGrupoViewController.swift
+//  TuAmi
+//
+//  Created by David Martínez Albaina on 19/12/15.
+//  Copyright © 2015 Egibide04. All rights reserved.
+//
+
+import UIKit
+
+class CrearGrupoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
